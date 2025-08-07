dd3ccc3b3631b247662073833d386fc01044b766
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-08-07 14:45:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-demo-policy"
   location = "Central India"
@@ -19,4 +23,50 @@ resource "azurerm_app_service" "app" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.plan.id
+}
+
+resource "azurerm_api_management" "apim" {
+  name                = "demo-apim-instance"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_name      = "Demo Publisher"
+  publisher_email     = "demo@example.com"
+  sku_name            = "Developer_1"
+}
+
+resource "azurerm_api_management_api" "api" {
+  name                = "demo-api"
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+  revision            = "1"
+  display_name        = "Demo API"
+  path                = "demo"
+  protocols           = ["https"]
+  import {
+    content_format = "swagger-link-json"
+    content_value  = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v2.0/json/petstore.json"
+  }
+}
+
+resource "azurerm_api_management_api_policy" "api_policy" {
+  api_name            = azurerm_api_management_api.api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+    <base />
+    <set-header name="X-Demo-Header" exists-action="override">
+      <value>Added by APIM</value>
+    </set-header>
+  </inbound>
+  <backend>
+    <base />
+  </backend>
+  <outbound>
+    <base />
+  </outbound>
+</policies>
+XML
 }
