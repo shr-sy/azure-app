c670fe2b2a8b10eca59f10e11a4ceca51e7d636c
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-08-07 15:07:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,25 +7,43 @@ resource "azurerm_service_plan" "plan" {
   name                = "plan-demo-policy"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
 resource "azurerm_app_service" "app" {
   name                = "app-policy-demo"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan.id
+  app_service_plan_id = azurerm_service_plan.plan.id
 }
 
 resource "azurerm_api_management" "apim" {
-  name                = "demo-apim-instance-new"  # changed to avoid 409 conflict
+  name                = "demo-apim-instance"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   publisher_name      = "Demo Publisher"
   publisher_email     = "demo@example.com"
   sku_name            = "Developer_1"
+}
+
+resource "azurerm_api_management_api" "api" {
+  name                = "demo-api"
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+  revision            = "1"
+  display_name        = "Demo API"
+  path                = "demo"
+  protocols           = ["https"]
+  import {
+    content_format = "swagger-link-json"
+    content_value  = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v2.0/json/petstore-expanded.json"
+  }
+}
+
+resource "azurerm_api_management_api_policy" "api_policy" {
+  api_name            = azurerm_api_management_api.api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+  xml_content         = file("${path.module}/policy.xml")
 }
