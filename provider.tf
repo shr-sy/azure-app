2dfb6073401fdce897164eb2289423ef5f5ca9b5
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-08-07 15:42:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 provider "azurerm" {
   features {}
 
-  client_id       = jsondecode(base64decode(var.azure_credentials)).clientId
-  client_secret   = jsondecode(base64decode(var.azure_credentials)).clientSecret
-  tenant_id       = jsondecode(base64decode(var.azure_credentials)).tenantId
-  subscription_id = jsondecode(base64decode(var.azure_credentials)).subscriptionId
+  client_id       = jsondecode(var.azure_credentials).clientId
+  client_secret   = jsondecode(var.azure_credentials).clientSecret
+  tenant_id       = jsondecode(var.azure_credentials).tenantId
+  subscription_id = jsondecode(var.azure_credentials).subscriptionId
 }
 
 variable "azure_credentials" {
+  description = "Azure credentials JSON stored in GitHub Secrets"
   type        = string
-  description = "Base64 encoded Azure credentials JSON"
 }
