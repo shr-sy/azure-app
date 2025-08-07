effdc348a4fce6d40a19098c002ca51f2d1dbe16
Shruti Gupta <shruti.gupta.com@gmail.com>
2025-08-07 11:27:30 +0530

    adding main.tf

---
+++ azure-app-policy-demo/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-demo-policy"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "plan-demo-policy"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "app-policy-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+}
