1c220b524c1def97c8962a8030ceaf910fa793a6
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-08-07 12:10:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,10 @@ resource "azurerm_app_service_plan" "plan" {
   name                = "plan-demo-policy"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Basic"
+    size = "B1"
   }
 }
 
