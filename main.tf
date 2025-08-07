c9d6e75425d07dcf2c43196999e5d9efe105e1ba
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-08-07 12:08:16 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-demo-policy"
+  location = "Central India"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "plan-demo-policy"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "app-policy-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+}
